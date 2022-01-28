30522ad610e380994b384f4ca82c1d619fe9f6ba
Kumar100966 <adrian.etwaroo@gmail.com>
2022-01-28 11:08:02 -0400

    Coordinated drum audio and animation

--- app.js
+++ app.js
@@ -12,17 +12,36 @@ class Drumkit {
     this.hihatDrums = document.querySelectorAll(".hihat-box");
     this.kickAudio = document.querySelector(".kick-sound");
     this.snareAudio = document.querySelector(".snare-sound");
-    this.hihatAudio = document.querySelector(".hihat");
+    this.hihatAudio = document.querySelector(".hihat-sound");
     this.playButton = document.querySelector(".play-button");
   }
 
   repeat() {
     let repeatingCounter = this.index % 8;
-    console.log(repeatingCounter);
+    const activeBoxes = document.querySelectorAll(
+      `.box${repeatingCounter + 1}`
+    );
+
+    activeBoxes.forEach((box) => {
+      box.style.animation = "boxScale .3s ease-in-out alternate 2";
+      if (box.classList.contains("kick-box-active")) {
+        this.kickAudio.currentTime = 0.1;
+        this.kickAudio.play();
+      }
+      if (box.classList.contains("snare-box-active")) {
+        this.snareAudio.currentTime = 0.1;
+        this.snareAudio.play();
+      }
+      if (box.classList.contains("hihat-box-active")) {
+        this.hihatAudio.currentTime = 0.1;
+        this.hihatAudio.play();
+      }
+    });
+
     this.index++;
   }
 
-  play() {
+  start() {
     if (!this.intervalID) {
       this.intervalID = window.setInterval(
         () => this.repeat(),
@@ -30,6 +49,7 @@ class Drumkit {
       );
       return;
     }
+
     window.clearInterval(this.intervalID);
     this.intervalID = null;
   }
@@ -37,4 +57,32 @@ class Drumkit {
 
 const drumkit = new Drumkit();
 
-drumkit.playButton.addEventListener("click", () => drumkit.play());
+drumkit.playButton.addEventListener("click", () => drumkit.start());
+
+drumkit.kickDrums.forEach((element) => {
+  element.addEventListener("click", () =>
+    element.classList.toggle("kick-box-active")
+  );
+  element.addEventListener(
+    "animationend",
+    () => (element.style.animation = "")
+  );
+});
+drumkit.snareDrums.forEach((element) => {
+  element.addEventListener("click", () =>
+    element.classList.toggle("snare-box-active")
+  );
+  element.addEventListener(
+    "animationend",
+    () => (element.style.animation = "")
+  );
+});
+drumkit.hihatDrums.forEach((element) => {
+  element.addEventListener("click", () =>
+    element.classList.toggle("hihat-box-active")
+  );
+  element.addEventListener(
+    "animationend",
+    () => (element.style.animation = "")
+  );
+});
